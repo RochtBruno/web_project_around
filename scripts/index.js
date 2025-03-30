041d4551cf65771da958d121fc37577687af1a3d
Bruno <bruno.taveira.r0.382@gmail.com>
2025-03-30 11:02:39 -0300

    criação de cards corrigida

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,3 @@
-
 import { Card } from "./Card.js";
 import { FormValidator } from "./FormValidator.js";
 import PopupWithImage from "./PopupWithImage.js";
@@ -9,28 +8,28 @@ import Section from "./Section.js";
 const initialCards = [
   {
     name: "Vale de Yosemite",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
   },
   {
     name: "Lago Louise",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
   },
   {
     name: "Montanhas Carecas",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
   },
   {
     name: "Latemar",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
   },
   {
     name: "Parque Nacional da Vanoise ",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg"
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
   },
   {
     name: "Lago di Braies",
-    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg"
-  }
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  },
 ];
 
 const validationConfig = {
@@ -38,14 +37,20 @@ const validationConfig = {
   submitButtonSelector: ".profile__modal-button",
   inactiveButtonClass: "button--disabled",
   inputErrorClass: "input-error",
-  errorClass: "error-message"
+  errorClass: "error-message",
 };
 
 const editProfileFormElement = document.querySelector(".profile__modal-form");
 const addCardFormElement = document.querySelector(".profile__modal-add-form");
 
-const editProfileValidator = new FormValidator(validationConfig, editProfileFormElement);
-const addCardValidator = new FormValidator(validationConfig, addCardFormElement);
+const editProfileValidator = new FormValidator(
+  validationConfig,
+  editProfileFormElement
+);
+const addCardValidator = new FormValidator(
+  validationConfig,
+  addCardFormElement
+);
 
 const editButton = document.querySelector(".profile__infos-edit");
 const addButton = document.querySelector(".profile__button-rectangle");
@@ -53,16 +58,14 @@ const addButton = document.querySelector(".profile__button-rectangle");
 editProfileValidator.enableValidation();
 addCardValidator.enableValidation();
 
-
 const userInfo = new UserInfo({
   nameSelector: ".profile__infos-title",
-  jobSelector: ".profile__infos-description"
+  jobSelector: ".profile__infos-description",
 });
 
 // -------------------------
 const popupWithImage = new PopupWithImage(".popup");
 popupWithImage.setEventListeners();
-
 
 function handleCardClick({ link, name }) {
   popupWithImage.open({ link, title: name });
@@ -79,12 +82,11 @@ const cardsSection = new Section(
     renderer: (item) => {
       const cardElement = createCard(item);
       cardsSection.addItem(cardElement);
-    }
+    },
   },
   ".cards"
 );
 cardsSection.renderItems();
-
 
 const profilePopup = new PopupWithForm(".profile__modal", (formData) => {
   userInfo.setUserInfo({ name: formData.name, job: formData.job });
@@ -98,7 +100,6 @@ editButton.addEventListener("click", () => {
   document.querySelector("#jobInput").value = currentUser.job;
   profilePopup.open();
 });
-
 
 const addCardPopup = new PopupWithForm(".profile__modal-add", (formData) => {
   const newCardData = { name: formData.title, link: formData.link };
